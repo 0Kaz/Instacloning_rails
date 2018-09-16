a692c85de061241f207c0370d27ae696f322624d
Omar Kaz <kazeb.omar@gmail.com>
2018-09-16 11:07:45 +0100

    added carrierwave config on uploaders

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   devise_for :users,
              path: '',
              path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
-             :controllers => {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
+             :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#home"
 
   resources :users, only: [:show]
+
+  resources :posts, only: [ :index, :show, :create ] do
+    resources :photos, only: [:create]
+  end
 end
