279b1ae26c2c12813ec79baab590acbca59f003d
Omar Kaz <kazeb.omar@gmail.com>
2018-09-23 10:22:52 +0100

    details added for the post

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users,
              path: '',
              path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
              :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
+  root to: "posts#index"
 
   resources :users, only: [:show]
 
