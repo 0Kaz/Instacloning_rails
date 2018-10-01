cd1be192db2ec5333edb745ebf993147c6db3821
Omar Kaz <kazeb.omar@gmail.com>
2018-10-01 18:26:20 +0100

    Comment model added

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
 
   has_many :photos, dependent: :destroy
   has_many :likes, -> { order(:created_at => :desc) }
-
+  has_many :comments, -> { order(:created_at => :desc) }
 
   def is_belong_to?(user)
     Post.find_by(user_id: user.id, id: id)
